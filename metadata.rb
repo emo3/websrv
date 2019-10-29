adf7f4bf7e7fa5a6feb2a94cc711d1fc88464d0f
Ed Overton <e.overton@gmail.com>
2019-10-28 21:29:32 -0400

    commits to 2019.10.28

--- metadata.rb
+++ metadata.rb
@@ -1,24 +1,16 @@
 name 'websrv'
-maintainer 'Ed Overton'
+source_url       'https://github.com/emo3/websrv'
+issues_url       'https://github.com/emo3/websrv/issues'
+maintainer	 'Ed Overton'
 maintainer_email 'infuse.1301@gmail.com'
-license 'Apache 2.0'
+chef_version     '>= 13.9'
+license          'Apache-2.0'
 description 'Installs/Configures websrv'
-long_description 'Installs/Configures websrv'
-version '1.1.0'
-chef_version '>= 13.0'
+version		 '1.2.0'
+
+supports 'redhat'
 supports 'centos'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/websrv/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/websrv'
 depends 'apache2'
 depends 'hostsfile'
 depends 'lvm'
