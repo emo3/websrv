08447400ae1100447d9329153ed940dd362a5563
Ed Overton <e.overton@gmail.com>
2020-02-17 23:24:13 -0500

    make more changes for rhel8

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['websrv']['rhel'] =
   if node['platform_version'] < '8'
     %w(yum-utils createrepo rsync bind-utils)
   else
-    %w(dnf-utils createrepo_c rsync bind-utils)
+    %w(yum-utils dnf-plugins-core rsync)
   end
 default['websrv']['chefsrv_ip']  = '10.1.1.10'
 default['websrv']['websrv_ip']   = '10.1.1.30'
