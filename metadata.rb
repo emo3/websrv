1cf2ea992a65a5036ac41f4bb1479226281443f9
Ed Overton <e.overton@gmail.com>
2021-09-25 01:12:25 -0400

    fix version and helper

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'you1@example.com'
 chef_version     '>= 15'
 license          'Apache-2.0'
 description      'Installs/Configures websrv'
-version          '1.5.0'
+version          '1.6.0'
 
 supports 'redhat'
 supports 'centos'
