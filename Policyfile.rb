c4f60dd41e60adf598dd586dbfab5be9f46e500a
Ed Overton <e.overton@gmail.com>
2023-06-03 18:15:59 -0400

    fix named run list

--- Policyfile.rb
+++ Policyfile.rb
@@ -11,7 +11,7 @@ default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
 run_list 'websrv::no-ssl'
-named_run_list :dossl, 'websrv::no-ssl'
+named_run_list :dossl, 'websrv::ssl'
 
 # Specify a custom source for a single cookbook:
 cookbook 'websrv', path: '.'
